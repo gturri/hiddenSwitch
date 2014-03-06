511c71f53b14cd163cea32f0cbc2eff56fba248b
hh.lohmann <hh.lohmann@yahoo.de>
2014-03-06 12:58:02 +0100

    German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* German language file
+*
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+* @author hh.lohmann <hh.lohmann@yahoo.de>
+*/
+ 
+$lang['encoding'] = 'utf-8';
+$lang['direction'] = 'ltr';
+$lang['default'] = 'Alle anzeigen / alle verstecken';
